df9113f37958fced33a02e86f49bd6619ac6ec25
chris B <gem49plus@gmail.com>
2025-05-10 04:42:21 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@
 # The `f` in the `get_requirements` function is used to open a file in read mode. The `open(path)`
 # function is used to open the file specified by the `path` parameter, and the `f` variable is
 # assigned the file object that is returned by the `open` function.
+# The variable `f` in the `get_requirements` function is used to store the file object returned by the
+# `open(path)` function. This file object represents the file specified by the `path` parameter and is
+# used to read the contents of the file.
 from setuptools import find_packages, setup
 
 
